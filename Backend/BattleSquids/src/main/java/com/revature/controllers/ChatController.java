18a9bcd0d344f96920e263eca0040cd31f57b3fd
muhammad_aown <aown619@gmail.com>
2020-12-23 12:02:46 -0500

    controllers all functions

--- Backend/BattleSquids/src/main/java/com/revature/controllers/ChatController.java
+++ Backend/BattleSquids/src/main/java/com/revature/controllers/ChatController.java
@@ -3,16 +3,20 @@ package com.revature.controllers;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.beans.Chat;
+import com.revature.beans.GameStatus;
 import com.revature.beans.TileStatus;
 import com.revature.services.ChatService;
 
@@ -50,5 +54,27 @@ public class ChatController {
 		}
 		return ResponseEntity.ok(result);
 	}
+	
+	@PutMapping(path="/{id}")
+	public ResponseEntity<Void> updateChat(HttpSession session, @PathVariable("id") Integer id, 
+			@RequestBody Chat chat) {
+		Chat ch = (Chat) session.getAttribute("id");
+		if (ch != null && ch.getId().equals(id)) {
+			serv.updateChat(chat);
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+	}
+	
+	 @DeleteMapping(value = "/{id}")
+	    public ResponseEntity<Void> deletechat(@PathVariable Integer id) {
+
+	    Chat gs= serv.getChatbyId(id);
+	    if(gs != null) {
+	    	serv.deleteChat(gs);
+	    	return ResponseEntity.ok().build();
+	    }
+	    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+	 }     
 
 }
