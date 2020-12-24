2e570507cffa70886594918484c3589a646b98b2
Jason Zelonka <jzelonka@ufl.edu>
2020-12-24 12:30:49 -0500

    stuff
    stuff

--- Frontend/BattleSquidsFrontApp/src/app/home/home.component.ts
+++ Frontend/BattleSquidsFrontApp/src/app/home/home.component.ts
@@ -8,8 +8,11 @@ import { PersonService } from '../services/person.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  personServ: PersonService;
 
-  constructor(personServ: PersonService) { }
+  constructor(personServ: PersonService) {
+    this.personServ = personServ;
+   }
 
   ngOnInit(): void {
   }
